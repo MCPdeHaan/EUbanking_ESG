e4db145e2c0805e1b30800ee0a53f58fc64fea8a
MCPdeHaan <mauritsdehaan.info@gmail.com>
2025-02-05 13:51:29 +0100

    decoding withing importing

--- R/importing.R
+++ R/importing.R
@@ -1,12 +1,24 @@
-library(readr); library(tidyverse); library(zoo)
+library(readr); library(readxl); library(dplyr)
 
 sovereign_debt <- read_csv("data/Sovereign debt exposures.csv")
 other_templates <- read_csv("data/Other templates.csv")
 credit_risk <- read_csv("data/Credit risk.csv")
 market_risk <- read_csv("data/Market risk.csv")
 
-# Credit risk
-# Function to convert LEI codes to bank name
-credit_risk <- convert_lei_to_bank_name(credit_risk)
-credit_risk <- convert_status_code(credit_risk)
-credit_risk <- convert_exposure_code(credit_risk)
+metadata <- read_excel("data/Metadata.xlsx", 
+                       sheet = "List of Institutions",
+                       skip = 1) %>%
+  select(LEI_code = 3, Bank_Name = 4) %>% 
+  rename(LEI_Code = LEI_code)
+
+credit_risk <- credit_risk %>%
+  left_join(metadata, by = "LEI_Code")
+
+credit_risk <- credit_risk %>%
+  relocate(Bank_Name, .after = LEI_Code)
+
+credit_risk <- credit_risk %>%
+  mutate(
+    Year = year(Period),  # Extract year
+    Month = month(Period) # Extract month
+  )
